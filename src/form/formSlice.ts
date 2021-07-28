07ae446f1c0837b2ee20a07ff7b1db9f1544bd31
kir3kin <kir3kin@gmail.com>
2021-07-28 21:29:45 +0200

    add new form bg

--- src/form/formSlice.ts
+++ src/form/formSlice.ts
@@ -4,92 +4,92 @@ import { RootState } from '../app/store'
 import { iInputType } from '../interfaces/interfaces'
 
 interface iFormState {
-  inputs: iInputType
-  loading: boolean
-  formDisable: boolean
-  logIn: {
-    msg: string
-    status: 'logged' | 'not logged'
-  }
+	inputs: iInputType
+	loading: boolean
+	formDisable: boolean
+	logIn: {
+		msg: string
+		status: 'logged' | 'not logged'
+	}
 }
 const initialState: iFormState = {
-  inputs: {},
-  loading: false,
-  formDisable: false,
-  logIn: {
-    msg: '',
-    status: 'not logged'
-  }
+	inputs: {},
+	loading: false,
+	formDisable: false,
+	logIn: {
+		msg: '',
+		status: 'not logged'
+	}
 }
 
 export const logIn = createAsyncThunk(
-  'form/sendFormData',
-  async (emptyStoreInputs: iInputType, thunkAPI) => {
-    const response = await sendFormData()
-    thunkAPI.dispatch(hideLogStatus())
-    return {
-      data: response.data,
-      emptyStoreInputs
-    }
-  }
+	'form/sendFormData',
+	async (emptyStoreInputs: iInputType, thunkAPI) => {
+		const response = await sendFormData()
+		thunkAPI.dispatch(hideLogStatus())
+		return {
+			data: response.data,
+			emptyStoreInputs
+		}
+	}
 )
 
 export const hideLogStatus = createAsyncThunk(
-  'form/hideLogStatus',
-  async () => {
-    await new Promise<string>((resolve) =>
-      setTimeout(() => resolve(''), 2000)
-    )
-  }
+	'form/hideLogStatus',
+	async () => {
+		await new Promise<string>((resolve) =>
+			setTimeout(() => resolve(''), 2000)
+		)
+	}
 )
 
 export const formSlice = createSlice({
-  name: 'form',
-  initialState,
-  reducers: {
-    changeInput(
-      state,
-      action: PayloadAction<iInputType>
-    ) {
-      state.inputs = {
-        ...state.inputs,
-        ...action.payload
-      }
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(logIn.pending, state => {
-        state.loading = true
-        state.formDisable= true
-      })
-      .addCase(logIn.fulfilled, (state, action) => {
-        state.loading = false
-        state.formDisable = false
-        state.inputs = {
-          ...state.inputs,
-          ...action.payload.emptyStoreInputs
-        }
-        state.logIn = {
-          msg: action.payload.data,
-          status: 'logged'
-        }
-      })
-      .addCase(hideLogStatus.fulfilled, state => {
-        state.logIn.status = 'not logged'
-      })
-  },
+	name: 'form',
+	initialState,
+	reducers: {
+		changeInput(
+			state,
+			action: PayloadAction<iInputType>
+		) {
+			state.inputs = {
+				...state.inputs,
+				...action.payload
+			}
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(logIn.pending, state => {
+				state.loading = true
+				state.formDisable= true
+			})
+			.addCase(logIn.fulfilled, (state, action) => {
+				state.loading = false
+				state.formDisable = false
+				state.inputs = {
+					...state.inputs,
+					...action.payload.emptyStoreInputs
+				}
+				state.logIn = {
+					msg: action.payload.data,
+					status: 'logged'
+				}
+			})
+			.addCase(hideLogStatus.fulfilled, state => {
+				state.logIn.status = 'not logged'
+			})
+	},
 })
 
 export const { changeInput } = formSlice.actions
 
 export const selectState = (state: RootState) => {
-  return {
-    inputs: state.form.inputs,
-    loading: state.form.loading,
-    formDisable: state.form.formDisable,
-    logIn: state.form.logIn
-  }
+	return {
+		inputs: state.form.inputs,
+		loading: state.form.loading,
+		formDisable: state.form.formDisable,
+		logIn: state.form.logIn
+	}
 }
 
 export default formSlice.reducer
